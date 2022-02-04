536dbd1ebc45b2c520788fb3ad70c1d2702e1ec5
pecasdev <pecasdev@gmail.com>
2022-02-04 16:47:23 -0500

    Add create_account endpoint, list_accounts endpoint, basic schema for postgres, Insomnia tests for endpoints (#6)

--- sigmabank/src/APIfunctions.js
+++ sigmabank/src/APIfunctions.js
@@ -1,8 +1,8 @@
-const GET_FETCH = {
+export const GET_FETCH = {
     method: "GET"
 }
 
-const POST_FETCH = {
+export const POST_FETCH = {
     method: "POST",
     headers: {"Content-Type": "application/json"}
 }
@@ -11,15 +11,10 @@ function endpoint(details) {
     return 'http://localhost:5000/' + details;
 }
 
-async function someFunction() {
-    var name = document.getElementById("fruit-name").value;
-    fetch(endpoint(`create_fruit/${name}`), POST_FETCH).then(resp => console.log(resp));
-}
-
 async function otherFunction() {
     fetch("http://localhost:5000/list_fruit")
     fetch(endpoint('list_fruit'), GET_FETCH).then(resp => resp.json())
                                             .then(json => console.table(json));
 }
 
-export default {someFunction, otherFunction};
+export {endpoint};
--- sigmabank/src/App.js
+++ sigmabank/src/App.js
@@ -1,21 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import API from './APIfunctions.js';
+import {endpoint, GET_FETCH} from './APIfunctions';
+import CreateAccountForm from './CreateAccountForm.js';
 
-/*async function someFunction() {
-  var resp = await fetch("http://localhost:5000/createFruit");
-  console.log(await resp.json());
-}*/
+async function listAccounts() {
+  var resp = await fetch(endpoint("list_accounts"), GET_FETCH);
+  console.table(await resp.json());
+}
 
-const VERSION_NUMBER = "0.1.0";
+const VERSION_NUMBER = "0.1.1";
 
 function App() {
   return (
     <div className="App">
-      <p>sup</p>
-      <input id="fruit-name" placeholder="Enter fruit name"></input>
-      <button onClick={API.someFunction}>Create Fruit</button>
-      <button onClick={API.otherFunction}>List all fruits (to console)</button>
+      <p>Create Account</p>
+      <CreateAccountForm />
+      <button onClick={listAccounts}>List Accounts</button>
       <footer>SigmaBank Version {VERSION_NUMBER}</footer>
     </div>
   );
