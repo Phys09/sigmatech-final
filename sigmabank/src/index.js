6c663543a9b4469f23fbb61fc5ff9a00294f41ed
Xinlei Xu <trojblue@gmail.com>
2022-02-10 08:18:30 -0500

    [SIG15] editing code  peripherals to match up with the backend

--- sigmabank/src/index.js
+++ sigmabank/src/index.js
@@ -7,14 +7,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import {AuthContextProvider} from "./context";
 import CreateAccountForm from "./routes/CreateAccountForm";
+import LoginForm from "./routes/LoginForm";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="CreateAccountForm" element={<CreateAccountForm />} />
-    </Routes>
-  </BrowserRouter>,
+  <AuthContextProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/signup" element={<CreateAccountForm />} />
+        <Route path="/login" element={<LoginForm />} />
+      </Routes>
+    </BrowserRouter>,
+  </AuthContextProvider>,
   document.getElementById("root")
 );
