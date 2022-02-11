a7b88a909cf89746d1d7b4028163884cace6a8fd
Xinlei Xu <38530926+Trojblue@users.noreply.github.com>
2022-02-10 21:04:02 -0500

    Revert "Sig 19"

--- sigmabank/src/context.js
+++ sigmabank/src/context.js
@@ -4,9 +4,8 @@ export const AuthContext = React.createContext();
 
 export const AuthContextProvider = ({children}) => {
     const [user, setUser] = useState(null);
-    const [username, setUsername] = useState(null);
     const [loggedin, setLoggedin] = useState(false);
-    const values = {user, setUser, loggedin, setLoggedin, username, setUsername};
+    const values = {user, setUser, loggedin, setLoggedin};
 
     return (
         <AuthContext.Provider value={values}>
