a83c2193fd88f7295f4ef6f42e79608bf77be946
Michael Kwan <michaelkwan1238@gmail.com>
2022-03-03 21:21:00 -0500

    Make empty div for page

---
+++ sigmabank/src/routes/LoanApplication.js
@@ -0,0 +1,14 @@
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import NavbarLogin from "../components/navbarLogin";
+import FooterMain from "../components/footer";
+import "../css/login.css";
+import "../css/App.css";
+
+export default function LoanApplication() {
+  return (
+    <div className="LoanApplication">
+
+    </div>
+  );
+}
