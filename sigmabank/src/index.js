069d5518ee54c24e82c3aec7f0f4fd8674a80bba
Michael Kwan <michaelkwan1238@gmail.com>
2022-03-17 20:11:45 -0400

    Populate loan application page

--- sigmabank/src/index.js
+++ sigmabank/src/index.js
@@ -3,30 +3,28 @@
  */
 
 import "./css/index.css";
+import AdminPanel from "./routes/AdminPanel";
+import App from "./App";
+import CreateAccountForm from "./routes/CreateAccountForm";
+import EditAccountForm from "./routes/EditAccountDetail";
+import LoanApplicationForm from "./routes/LoanApplication";
+import LoginForm from "./routes/LoginForm";
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
-import {AuthContextProvider} from "./context";
-import CreateAccountForm from "./routes/CreateAccountForm";
-import LoginForm from "./routes/LoginForm";
-
 import TransactionHistory from "./routes/TransactionHistory";
-import EditAccountForm from "./routes/EditAccountDetail";
-import AdminPanel from "./routes/AdminPanel";
-import LoanApplication from "./routes/LoanApplication";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {AuthContextProvider} from "./context";
 
 ReactDOM.render (
   <AuthContextProvider>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/loans" element={<LoanApplication />} />
+        <Route path="/loans" element={<LoanApplicationForm />} />
         <Route path="/signup" element={<CreateAccountForm />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/transactions" element={<TransactionHistory />} />
         <Route path="/admin" element={<AdminPanel />} />
-        <Route path="/loginform" element={<LoginForm />} />
         <Route path="/edit" element={<EditAccountForm />} />
       </Routes>
     </BrowserRouter>
--- sigmabank/src/routes/LoanApplication.js
+++ sigmabank/src/routes/LoanApplication.js
@@ -2,13 +2,54 @@ import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import NavbarLogin from "../components/navbarLogin";
 import FooterMain from "../components/footer";
-import "../css/login.css";
+import "../css/LoanApplication.css";
 import "../css/App.css";
+import NavbarMain from "../components/navbar";
 
-export default function LoanApplication() {
-  return (
-    <div className="LoanApplication">
+export default class LoanApplicationForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loanType: "", // type of loan customer is applying for
+      reasoning: "", // reason for requesting a loan
+      amount: 0.0, // the amount they're requesting
+    };
 
-    </div>
-  );
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    alert("Loan applications currently not yet implemented");
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="LoanPage">
+        <NavbarMain />
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Loan Type
+            <select className="select-loan-type" value={this.state.value} onchange={this.handleChange}>
+              <option value="business">Business Loan</option>
+              <option value="mortgage">Mortgage Loan</option>
+              <option value="personal">Personal Loan</option>
+            </select>
+            <input
+              type="text"
+              placeholder="Reason for application"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
 }
