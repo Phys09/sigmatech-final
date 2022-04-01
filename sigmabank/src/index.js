755c0e6f996705bbb21c5588b7bc0c1b05037c22
Michael Kwan <michaelkwan1238@gmail.com>
2022-03-31 21:04:12 -0400

    Sig 7 (#68)
    * Create initial LoanApplication files for page
    
    * Add add LoanApplication page to react router
    
    * Make empty div for page
    
    * Add loan application page to routing
    
    * Populate loan application page
    
    * Update LoanApplication.js
    
    * Allows page to check if form is valid
    
    * Fix cookies
    
    * Finish loan application implementation
    
    * Fix css for loan application page

--- sigmabank/src/index.js
+++ sigmabank/src/index.js
@@ -1,30 +1,31 @@
-/** 
+/**
  * This file is dedicated to rendering the entire application
  */
 
-import React from "react";
-import { CookiesProvider } from "react-cookie";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "./css/index.css";
 import AdminPanel from "./routes/AdminPanel";
+import App from "./App";
+import BankStats from "./routes/BankStats";
 import ChatPage from "./routes/ChatPage";
 import CreateAccountForm from "./routes/CreateAccountForm";
 import EditAccountForm from "./routes/EditAccountDetail";
+import LoanApplicationForm from "./routes/LoanApplication";
 import LoginForm from "./routes/LoginForm";
-import {MakeTransaction, SendMoney} from  "./routes/MakeTransaction";
+import React from "react";
+import ReactDOM from "react-dom";
 import TransactionHistory from "./routes/TransactionHistory";
-import BankStats from "./routes/BankStats";
-
-import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import {SetupAutoPay} from "./routes/makeAutopay";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
+import { MakeTransaction, SendMoney } from "./routes/MakeTransaction";
+import { SetupAutoPay } from "./routes/makeAutopay";
 
 ReactDOM.render(
   <CookiesProvider>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/loans" element={<LoanApplicationForm />} />
         <Route path="/signup" element={<CreateAccountForm />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/admin" element={<AdminPanel />} />
@@ -33,10 +34,8 @@ ReactDOM.render(
         <Route path="/chat" element={<ChatPage />} />
         <Route path="/transactions" element={<TransactionHistory />} />
         <Route path="/makeAutopay" element={<SetupAutoPay />}/>
-
         <Route path="/makeTransactions" element={<MakeTransaction />}>
-        <Route path="sendMoney" element={<SendMoney />}/>
-
+          <Route path="sendMoney" element={<SendMoney />} />
         </Route>
       </Routes>
     </BrowserRouter>
